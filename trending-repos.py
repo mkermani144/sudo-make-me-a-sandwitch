8609784b7f24271d99f8ef1dd030b6425629f5c8
mohammad <mkermani144@gmail.com>
2016-11-26 11:26:40 +0330

    Do not show repositories you have seen before

--- trending-repos.py
+++ trending-repos.py
@@ -3,11 +3,18 @@ import bs4
 import webbrowser
 import os
 import sys
-sys.stdout = open(os.devnull, 'w')
-html = requests.get('https://github.com/trending')
-html.raise_for_status()
-soup = bs4.BeautifulSoup(html.text)
-rel_urls = soup.select('.repo-list > li > div > h3 > a')[:6]
-urls = ['https://github.com' + rel_url.attrs['href'] for rel_url in rel_urls]
-for url in urls:
-    webbrowser.open(url)
+if not os.path.isfile('repos.list'):
+    f = open('repos.list', 'w+')
+    f.close()
+with open('repos.list', 'r+') as repos_list:
+    repos = repos_list.read().splitlines()
+    sys.stdout = open(os.devnull, 'w')
+    html = requests.get('https://github.com/trending')
+    html.raise_for_status()
+    soup = bs4.BeautifulSoup(html.text)
+    elements = soup.select('.repo-list > li > div > h3 > a')[:6]
+    rel_urls = [el.attrs['href'] for el in elements]
+    urls = [rel_url for rel_url in rel_urls if rel_url not in repos]
+    for url in urls:
+        webbrowser.open('https://github.com' + url)
+        repos_list.write(url + '\n')
