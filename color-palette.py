4f26fac210caa4bd74725eb86c65247201b88ffb
mohammad <mkermani144@gmail.com>
2017-01-29 08:31:46 +0330

    Add image resize option for better performance

--- color-palette.py
+++ color-palette.py
@@ -24,14 +24,26 @@ parser.add_option(
     default=50,
     type='int',
     help='set treshold of difference between colors')
+parser.add_option(
+    '-z', '--zoom-out',
+    dest='zoomout',
+    default=0,
+    type='int',
+    help='set how much the image should be zoomed out')
 
 options, args = parser.parse_args()
 treshold = options.treshold
 palette_length = options.palette_length
+zoomout = options.zoomout
 if len(args) == 0:
     parser.error('No image specified.')
 
 image = Image.open(args[-1])
+if zoomout == 0:
+    ratio = min(256/image.size[0], 256/image.size[1])
+else:
+    ratio = 1/zoomout
+image.thumbnail((image.size[0]*ratio, image.size[1]*ratio), Image.ANTIALIAS)
 image_pixels = image.getdata()
 image_pixels = [(x[0], x[1], x[2]) for x in image_pixels]
 image_dict = dict(counter(image_pixels))
