287a77e60a6d6f91ec04b5bc0dc13e6fff0cf8e1
mohammad <mkermani144@gmail.com>
2017-01-29 12:59:47 +0330

    Change option names

--- color-palette.py
+++ color-palette.py
@@ -25,24 +25,25 @@ parser.add_option(
     type='int',
     help='set treshold of difference between colors')
 parser.add_option(
-    '-z', '--zoom-out',
-    dest='zoomout',
-    default=0,
+    '-s', '--scale-down',
+    dest='scaledown',
+    default=1,
     type='int',
-    help='set how much the image should be zoomed out')
+    help='set how much the image should be scaled down')
 
 options, args = parser.parse_args()
 treshold = options.treshold
 palette_length = options.palette_length
-zoomout = options.zoomout
+scaledown = options.scaledown
 if len(args) == 0:
     parser.error('No image specified.')
 
 image = Image.open(args[-1])
-if zoomout == 0:
-    ratio = min(256/image.size[0], 256/image.size[1])
-else:
-    ratio = 1/zoomout
+try:
+    ratio = 1/scaledown
+except Exception as e:
+    print(e)
+    exit(1)
 image.thumbnail((image.size[0]*ratio, image.size[1]*ratio), Image.ANTIALIAS)
 image_pixels = image.getdata()
 image_pixels = [(x[0], x[1], x[2]) for x in image_pixels]
